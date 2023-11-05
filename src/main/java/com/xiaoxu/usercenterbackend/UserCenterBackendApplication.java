10bcf9554f051b5ab135f84949b50520fc61c9b1
xiaoxu954 <x17605049954@163.com>
2023-11-05 16:02:23 +0800

    项目初始化

---
+++ src/main/java/com/xiaoxu/usercenterbackend/UserCenterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoxu.usercenterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterBackendApplication.class, args);
+    }
+
+}
