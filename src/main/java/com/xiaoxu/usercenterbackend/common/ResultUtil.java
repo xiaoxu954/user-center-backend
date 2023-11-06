7a549214ec56e2f7b94c3e9c626324660b7154f0
xiaoxu954 <x17605049954@163.com>
2023-11-06 23:57:58 +0800

    自定义错误码

--- src/main/java/com/xiaoxu/usercenterbackend/common/ResultUtil.java
+++ src/main/java/com/xiaoxu/usercenterbackend/common/ResultUtil.java
@@ -1,7 +1,17 @@
 package com.xiaoxu.usercenterbackend.common;
 
 public class ResultUtil {
+    /**
+     * 成功
+     * @param data
+     * @param <T>
+     * @return
+     */
     public static <T> BaseResponse<T> success(T  data) {
         return new BaseResponse<>(0, data, "ok");
     }
+    public static <T> BaseResponse<T> error(ErrorCode errorCode) {
+        return new BaseResponse<>(errorCode);
+    }
+
 }
